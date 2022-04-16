ef488ce520f3ce5a981900728851f23324fbe937
bruce <gotoarmy@naver.com>
2022-04-17 01:51:09 +0900

    문자열 길이 큰거 출력

---
+++ code/src/string/findalpha.java
@@ -0,0 +1,38 @@
+package string;
+
+import java.util.Scanner;
+
+class findalpha {
+	
+	char[] alphain =new char[100];
+	Scanner in =new Scanner(System.in);
+	String input1 = in.nextLine();
+    char input2 = in.next().charAt(0);
+    int count = input1.length();
+    int iinput2 = input2;
+    int alphacount=0;
+    {
+	for (int i =0; i < count; i++)
+    {
+    	 alphain[i]= input1.charAt(i); // 소문자 아스키 값이들어감
+    	 int j = alphain[i];
+    	 if (j < 93) //대문자
+    	 {
+    		 if(j == iinput2 || j+32 == iinput2)
+    		 {
+    			 alphacount++;
+    		 }
+    	 }
+    	 else if(j > 93) //소문자
+    	 {
+    		 if(j == iinput2 || j-32 == iinput2)
+    		 {
+    			 alphacount++;
+    		 }
+    	 }
+    	 
+   	 }
+    	System.out.print(alphacount);
+   }
+    //아스키 값이 들어감
+}

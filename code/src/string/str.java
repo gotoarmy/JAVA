67bbd82357a3cf83c78d0ce8b2b8f99ed2de3ce8
bruce <gotoarmy@naver.com>
2022-04-16 14:23:05 +0900

    대소문자변환 ,알파뱃찾기

--- code/src/string/str.java
+++ code/src/string/str.java
@@ -1,48 +1,51 @@
 package string;
 import java.util.Scanner;
 import java.io.*;
+
+
+class Up{
+	public String solution(String str)
+	{
+		int icount =0;
+		String answer =null;
+		for(char x : str.toCharArray())
+		{
+			if (x > 91) //대문자
+			{
+				x -=32;
+				System.out.print(x);
+				x +=32;
+			}
+			
+			if (x < 91) //소문자
+				{
+				x +=32; 
+				System.out.print(x);
+				}
+			
+			
+			
+		}
+		return answer;
+	}
+}
+
 public class str {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//입력을 넣는다
-		//1.string 배열을 입력받음
-		
-		char[] alphain =new char[100];
-		Scanner in =new Scanner(System.in);
-		String input1 = in.nextLine();
-	    char input2 = in.next().charAt(0);
-	    int count = input1.length();
-	    int iinput2 = input2;
-	    int alphacount =0;
+		Scanner in=new Scanner(System.in);
+		String an = in.next();
+		String bn = null;
+	    Up ln = new Up();
+	    bn = ln.solution(an);
 	    
-	    for (int i =0; i < count; i++)
-	    {
-	    	 alphain[i]= input1.charAt(i); // 소문자 아스키 값이들어감
-	    	 int j = alphain[i];
-	    	 if (j < 93) //대문자
-	    	 {
-	    		 if(j == iinput2 || j+32 == iinput2)
-	    		 {
-	    			 alphacount++;
-	    		 }
-	    	 }
-	    	 else if(j > 93) //소문자
-	    	 {
-	    		 if(j == iinput2 || j-32 == iinput2)
-	    		 {
-	    			 alphacount++;
-	    		 }
-	    	 }
-	    	 
-	   	 }
-	    	System.out.print(alphacount);
-	   }
-	    //아스키 값이 들어감
+	    
 	    
 		
 		
 	}
+}
 
 

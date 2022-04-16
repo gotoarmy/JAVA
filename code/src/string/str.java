edfbeebe8c32c19a621b3e62a44772138dc76ef3
bruce <gotoarmy@naver.com>
2022-04-17 01:49:50 +0900

    문자열 길이 큰거 출력

--- code/src/string/str.java
+++ code/src/string/str.java
@@ -3,48 +3,26 @@ import java.util.Scanner;
 import java.io.*;
 
 
-class Up{
-	public String solution(String str)
-	{
-		int icount =0;
-		String answer =null;
-		for(char x : str.toCharArray())
-		{
-			if (x > 91) //대문자
-			{
-				x -=32;
-				System.out.print(x);
-				x +=32;
-			}
-			
-			if (x < 91) //소문자
-				{
-				x +=32; 
-				System.out.print(x);
-				}
-			
-			
-			
-		}
-		return answer;
-	}
-}
+
 
 public class str {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Scanner in=new Scanner(System.in);
-		String an = in.next();
-		String bn = null;
-	    Up ln = new Up();
-	    bn = ln.solution(an);
-	    
-	    
+		 Scanner in =new Scanner(System.in);
+		    String input1 = in.nextLine();
+		    split an = new split();
+		    System.out.println(an.solution1(input1));
+		    return ;
 	    
 		
 		
+	}
+
+	private static char[] solution(String input1) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
 

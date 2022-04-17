eca62d2c08ada1391638ed926da44cb4be49f211
bruce <gotoarmy@naver.com>
2022-04-17 21:43:53 +0900

    문자열뒤집기

--- code/src/string/str.java
+++ code/src/string/str.java
@@ -4,26 +4,15 @@ import java.io.*;
 
 
 
-
 public class str {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		 Scanner in =new Scanner(System.in);
-		    String input1 = in.nextLine();
-		    split an = new split();
-		    System.out.println(an.solution1(input1));
-		    return ;
-	    
-		
-		
+		 
+		    return ;   
 	}
-
-	private static char[] solution(String input1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
 }
 
 

e8caa9dcc5950a1c0457908b7fc4740895658be6
bruce <gotoarmy@naver.com>
2022-04-24 17:14:23 +0900

    문자와과 인덱스의 거리를 출력

--- code/src/string/Strmain.java
+++ code/src/string/Strmain.java
@@ -7,9 +7,11 @@ public class Strmain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in=new Scanner(System.in);
-	    String input1 = in.nextLine();	    
-	    OnlyNumber Onlynum = new OnlyNumber();	    
-	    System.out.println(Onlynum.solu(input1));	    	    
+	    String input1 = in.nextLine();
+	    String[] input2 =input1.split(" ");
+	    char c_input = input2[1].charAt(0);
+	    Shortestdistance Onlynum = new Shortestdistance();	    
+	    Onlynum.solu(input2[0], c_input);	    	    
 	    return ;
 	}
 
